2fb27557a9c5f14d23921ab69c02e2648748bd56
Glenn Ludszuweit <glenn.ludszuweit@gmail.com>
2021-03-12 14:41:17 +0100

    Updates

---
+++ precache-manifest.7587999b417fc981561260ff3e47efbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81aa6dfa249329d03021c34b799fe1df",
+    "url": "/agora-test/index.html"
+  },
+  {
+    "revision": "df47fadd4c3625fae729",
+    "url": "/agora-test/static/css/main.5d543891.chunk.css"
+  },
+  {
+    "revision": "6876b2b43aa443250917",
+    "url": "/agora-test/static/js/2.fb824ce0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/agora-test/static/js/2.fb824ce0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df47fadd4c3625fae729",
+    "url": "/agora-test/static/js/main.9b6326ec.chunk.js"
+  },
+  {
+    "revision": "7aa5e06cc69869390199",
+    "url": "/agora-test/static/js/runtime-main.4acb0f70.js"
+  }
+]);
